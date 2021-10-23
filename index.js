0e47348118caf0bd98d24736fe3bb9c6b49e3dab
Farhad74 <evgeniy_mgn_74@mail.ru>
2021-10-23 19:11:22 +0300

    web server is created

--- index.js
+++ index.js
@@ -1,12 +1,64 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((request, response) => {
-    console.log(request.url)
-    const el = "<h1";
-    const el2 = ">hello Nodejs</";
-    const el3 = "h1>"
-    response.write(el+el2+el3);
-    response.end()
+    if (request.method === 'GET') {
+        response.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+        if (request.url === '/') {
+            fs.readFile(
+                path.join(__dirname, 'views', 'index.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        response.end(content);
+                    }
+                }
+            )
+        } else if (request.url === '/about') {
+            fs.readFile(
+                path.join(__dirname, 'views', 'about.html'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        response.end(content);
+                    }
+                }
+            )
+        } else if (request.url === '/api/users') {
+            response.writeHead(200, {
+                "Content-Type": "text/json"
+            });
+            const users = [{
+                name: 'jhon',
+                age: 25
+            },{
+                name: 'oleg',
+                age: 29
+            }];
+            response.end(JSON.stringify(users));
+        }
+    } else if (request.method === "POST") {
+        const body = [];
+        request.on('data', data => {
+            body.push(Buffer.from(data));
+        });
+        response.writeHead(200, {
+            "content-type": "text/html"
+        });
+        request.on('end', () => {
+            const message = (body.toString().split('=')[1]);
+            response.end(`
+        <h1>Your Message: ${message}</h1>
+        `)
+        });
+    }
 });
 
 server.listen(3000, () => {
